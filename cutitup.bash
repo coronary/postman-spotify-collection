9e39fc41268ac799ea240ab10ec31d0e878f4932
Collin D <cjdekalb@gmail.com>
2021-04-06 22:06:09 -0700

    added comment

--- cutitup.bash
+++ cutitup.bash
@@ -1,3 +1,5 @@
+# script to add sections to each endpoint. 
+# Added group sections manually since there weren't that many
 clearfix="<div class=\"clearfix\"><\/div>"
 console="Try in our Web Console<\/a><\/p>"
 section_start="$clearfix\n<section class=\"endpoint\">"
